ae27bfb8d1b52e9313890949c7e45c879f70da69
Juvenescent9 <ambikaawasthimishra@gmail.com>
2025-07-29 20:38:16 -0500

    Update code.py

--- firmware/code.py
+++ firmware/code.py
@@ -13,9 +13,9 @@ keyboard = KMKKeyboard()
 # --- MATRIX SETUP ---
 keyboard.row_pins = (board.GP0, board.GP1, board.GP2, board.GP3, board.GP4, board.GP5)
 keyboard.col_pins = (
-    board.GP6, board.GP7, board.GP8, board.GP9, board.GP10, board.GP11,
+    board.GP7, board.GP8, board.GP9, board.GP10, board.GP11,
     board.GP12, board.GP13, board.GP14, board.GP15, board.GP16, board.GP17,
-    board.GP18, board.GP19, board.GP20, board.GP21
+    board.GP18, board.GP19, board.GP20, board.GP21, board.GP6
 )
 keyboard.diode_orientation = DiodeOrientation.COL2ROW
 
